429d3b1c36b75d1dd5a3069d2748ae5ff3a8afd8
Юрий Ищенко <m_o_r_94@mail.ru>
2015-09-30 11:22:52 +1000

    OOP

---
+++ Лабораторная 2/NumberGroupException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Лабораторная_2
+{
+    [Serializable]
+    class NumberGroupException : ApplicationException
+    {
+        public NumberGroupException() { }
+        public NumberGroupException(string message) : base(message) { }
+        public NumberGroupException(string message, Exception ex) : base(message) { }
+        // Конструктор для обработки сериализации типа
+        protected NumberGroupException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext contex)
+            : base(info, contex) { }
+    }
+}
---
+++ Лабораторная 2/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Лабораторная_2
+{
+    class Test:IDateAndCopy
+    {
+        public DateTime Born { get; set; }
+        public string nameSubject { get; set; }
+        public bool signExamination { get; set; }
+        public Test(string nameSubject, bool signExamination , DateTime Date)
+        {
+            this.nameSubject = nameSubject;
+            this.signExamination = signExamination;
+           this.Born = Date;
+        }
+        public Test()
+        {
+            this.nameSubject = "Предмет не определен";
+            this.signExamination = false;
+            Born = new DateTime (2000, 1, 1);
+        }
+        public override string ToString()
+        {
+            base.ToString();
+            return nameSubject + ", зачет: " + signExamination;
+        }
+        public object DeepCopy()
+        {
+            Test t = new Test();
+            t.nameSubject = this.nameSubject;
+           t.signExamination = this.signExamination;
+           t.Born = this.Born;
+            return t;
+        }
+    }
+}
